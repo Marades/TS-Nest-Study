0a2df1ff9d9d7ceef5dd72232c0b4fc09ccfec5d
Kim GeonHoi <jkl9510357@gmail.com>
2022-05-10 22:21:08 +0900

    socket io adapter설정

--- nest-socket/src/main.ts
+++ nest-socket/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { SocketIoAdapter } from './adapters/socket-io.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new SocketIoAdapter(app));
+
   await app.listen(3000);
 }
 bootstrap();
