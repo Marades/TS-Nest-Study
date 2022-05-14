a0af9b093a9d890babb0f0bef890fc6a118f1769
Kim GeonHoi <jkl9510357@gmail.com>
2022-05-14 21:24:28 +0900

    Prisma기반 CRUD 작성

--- nest-prisma/src/app.module.ts
+++ nest-prisma/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PostService],
 })
 export class AppModule {}
--- nest-prisma/src/prisma/prisma.module.ts
+++ nest-prisma/src/prisma/prisma.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { PrismaController } from './prisma.controller';
 
 @Module({
-  controllers: [PrismaController],
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
