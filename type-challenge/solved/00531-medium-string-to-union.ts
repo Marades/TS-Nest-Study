1cb915e8b5e1374e6392cc3efb9ae5eeada0fe6a
marades <jkl9510357@gmail.com>
2023-05-02 18:42:12 +0900

    [type-challenge] ~20230502

---
+++ type-challenge/solved/00531-medium-string-to-union.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<StringToUnion<"">, never>>,
+  Expect<Equal<StringToUnion<"t">, "t">>,
+  Expect<Equal<StringToUnion<"hello">, "h" | "e" | "l" | "l" | "o">>,
+  Expect<
+    Equal<
+      StringToUnion<"coronavirus">,
+      "c" | "o" | "r" | "o" | "n" | "a" | "v" | "i" | "r" | "u" | "s"
+    >
+  >
+];
+
+// ============= Your Code Here =============
+type StringToUnion<T extends string, U = never> = T extends ""
+  ? U
+  : T extends `${infer P}${infer R}`
+  ? StringToUnion<R, U | P>
+  : never;
