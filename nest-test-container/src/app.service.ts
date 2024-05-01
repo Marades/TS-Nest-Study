6f179d5051a8c6f640734fa8149f8950a29edd5d
marades <jkl9510357@gmail.com>
2024-05-01 18:11:40 +0900

    setting prisma simple api

--- nest-test-container/src/app.service.ts
+++ nest-test-container/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getHello(): Promise<string> {
+    const user = await this.prismaService.user.findFirst();
+    console.log(user);
+    return `Hello World! - ${user.name}`;
   }
 }
---
+++ nest-test-container/src/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient, Prisma } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['error', 'info', 'query', 'warn'],
+    });
+  }
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+}
